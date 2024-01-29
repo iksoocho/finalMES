0ca94e30b427761caa19761a28393187108087a8
iksoocho <jogaeya@gmail.com>
2024-01-29 14:47:37 +0900

    asdfasdfasdf

--- finalMES/src/main/java/com/example/demo/securingweb/MvcConfig.java
+++ finalMES/src/main/java/com/example/demo/securingweb/MvcConfig.java
@@ -13,7 +13,7 @@ public class MvcConfig implements WebMvcConfigurer  {
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/login").setViewName("login");
 		registry.addViewController("/empList").setViewName("emp/empList");
-		//asdasds
+		//asdasdsadfgadfgasdfg
 	}
 
 }
