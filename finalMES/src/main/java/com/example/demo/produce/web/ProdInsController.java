de2f00b786ed72c9337263d0b52daa018e544ce1
Amin2-hub <wnstjd05@naver.com>
2024-01-31 21:14:02 +0900

    [김준성]0131 클릭이벤트

--- finalMES/src/main/java/com/example/demo/produce/web/ProdInsController.java
+++ finalMES/src/main/java/com/example/demo/produce/web/ProdInsController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.produce.ProdInsDetailVO;
 import com.example.demo.produce.ProdInsVO;
 import com.example.demo.produce.service.ProdInsService;
 
@@ -27,9 +30,21 @@ public class ProdInsController {
 	@GetMapping("/planOrderList")
 	public String insAllList(Model model){
 		List<ProdInsVO> list = prodInsService.getInsList();
+		String code = null;
+	    if (!list.isEmpty()) {
+	        ProdInsVO firstProdInsVO = list.get(0); // 첫 번째 ProdInsVO 추출
+	        code = firstProdInsVO.getInsCode();
+	    }
+		List<ProdInsDetailVO> detailList = prodInsService.getInsDetailList(code);
 		model.addAttribute("insList", list);
-		
+		model.addAttribute("detailList", detailList);
 		return "produce/planOrderList";
+	}
+	
+	@GetMapping("/planOrderDetail")
+	@ResponseBody
+	public List<ProdInsDetailVO> getPlanOrderDetailList(@RequestParam String insCode){
+		return prodInsService.getInsDetailList(insCode);
 	}
 	
 }
