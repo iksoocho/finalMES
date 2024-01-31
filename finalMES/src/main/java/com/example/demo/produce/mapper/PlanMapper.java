afa33f4d6ca0f7f0e187c1db5ab6ece1528aa607
iksoocho <jogaeya@gmail.com>
2024-01-31 17:46:10 +0900

    asdfadsf

--- finalMES/src/main/java/com/example/demo/produce/mapper/PlanMapper.java
+++ finalMES/src/main/java/com/example/demo/produce/mapper/PlanMapper.java
@@ -2,10 +2,12 @@ package com.example.demo.produce.mapper;
 
 import java.util.List;
 
+import com.example.demo.produce.PlanOrdDVO;
 import com.example.demo.produce.PlanOrdVO;
 import com.example.demo.produce.PlanVO;
 
 public interface PlanMapper {
 	public List<PlanVO> selectPlanList();
 	public List<PlanOrdVO> selectOrdList();
+	public List<PlanOrdDVO> selectOrdDList(String ordCode);
 }
--- finalMES/src/main/java/com/example/demo/produce/service/impl/PlanServiceImpl.java
+++ finalMES/src/main/java/com/example/demo/produce/service/impl/PlanServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.produce.PlanOrdDVO;
+import com.example.demo.produce.PlanOrdVO;
 import com.example.demo.produce.PlanVO;
 import com.example.demo.produce.mapper.PlanMapper;
 import com.example.demo.produce.service.PlanService;
@@ -15,10 +17,24 @@ public class PlanServiceImpl implements PlanService{
 	@Autowired
 	PlanMapper planMapper;
 	
+	
+	
 	@Override
 	public List<PlanVO> getPlanList() {
 		// TODO Auto-generated method stub
 		return planMapper.selectPlanList();
+	}
+
+	@Override
+	public List<PlanOrdVO> getOrdList() {
+		// TODO Auto-generated method stub
+		return planMapper.selectOrdList();
+	}
+
+	@Override
+	public List<PlanOrdDVO> getOrdDList(String ordCode) {
+		// TODO Auto-generated method stub
+		return planMapper.selectOrdDList(ordCode);
 	}
 
 }
