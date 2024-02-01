3afcbe8a0b8b67d1306da7d286aa696ed464118f
현진 <148922882+KangHyunJin2@users.noreply.github.com>
2024-02-01 10:15:29 +0900

    [강현진] 커밋

---
+++ finalMES/src/main/java/com/example/demo/material/MatOrderInfoVO.java
@@ -0,0 +1,18 @@
+package com.example.demo.material;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class MatOrderInfoVO {
+	private String matOrInfoCode;
+	private String matOrCode;
+	private String matCode;
+	private int matOriCount;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date matOrDueDate;
+	
+}
--- finalMES/src/main/java/com/example/demo/material/service/MaterialService.java
+++ finalMES/src/main/java/com/example/demo/material/service/MaterialService.java
@@ -2,14 +2,17 @@ package com.example.demo.material.service;
 
 import java.util.List;
 
+import com.example.demo.material.MatOrderInfoVO;
 import com.example.demo.material.MatOrderVO;
 
 public interface MaterialService {
 	// 단건
 	public MatOrderVO getMat(String matCode);
 	
-	//전체
-	public List<MatOrderVO> getMaterialList(MatOrderVO materialVO);
+	// 발주 전체 모달
+	public List<MatOrderVO> getMaterialOrderList();
+	public List<MatOrderInfoVO> getMaterialOrdInfoList(String matordCode);
+	
 	
 	// 등록
 	public int insertMaterialOrder(MatOrderVO matOrderVO);
