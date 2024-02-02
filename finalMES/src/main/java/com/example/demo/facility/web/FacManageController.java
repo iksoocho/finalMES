a9c390947943b38f7400c2a9c5af356263aa63fb
parkhyunn <148922933+parkhyunn@users.noreply.github.com>
2024-02-02 21:24:24 +0900

    파스쿠찌 박규현

--- finalMES/src/main/java/com/example/demo/facility/web/FacManageController.java
+++ finalMES/src/main/java/com/example/demo/facility/web/FacManageController.java
@@ -1,16 +1,16 @@
 package com.example.demo.facility.web;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import com.example.demo.facility.FacManageVO;
 import com.example.demo.facility.mapper.FacManageMapper;
