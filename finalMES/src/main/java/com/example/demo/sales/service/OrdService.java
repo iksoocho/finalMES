49f197612412d7ca05c8f96685974b56b01780f8
윤성오 <nsa30003@gmail.com>
2024-02-06 08:59:30 +0900

    # WARNING: head commit changed in the meantime
    [윤성오] 2024-02-05

--- finalMES/src/main/java/com/example/demo/sales/service/OrdService.java
+++ finalMES/src/main/java/com/example/demo/sales/service/OrdService.java
@@ -16,17 +16,19 @@ public interface OrdService {
 
 	OrderDetailVO getOrderDetail(String ordDCode);
 
-	void insertOrder(OrderVO orderVO);
+	void saveOrder(OrderVO orderVO);
 
-	void insertOrderDetail(OrderDetailVO orderDetailVO);
+	void saveOrderDetail(OrderDetailVO orderDetailVO);
 
 	List<OrderVO> getOrderList(OrderVO orderVO);
 
 	List<OrderDetailVO> getOrderDetailList(OrderDetailVO orderDetailVO);
-	
+
 	List<BusinessListVO> getBusinessList(); // 거래처 목록
-	
+
 	OrderVO getOrderCode(String businessCode); // 거래처에 대한 가장 최근 주문서 불러오기
-	
+
 	List<ProductListVO> getProductList();
+	
+
 }
