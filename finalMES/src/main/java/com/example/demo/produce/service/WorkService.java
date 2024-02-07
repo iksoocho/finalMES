a9de8692cd3775e4852372898ead22d3790da4ea
Amin2-hub <wnstjd05@naver.com>
2024-02-07 17:34:13 +0900

    [김준성] 작업중간등록 인서트완료 조건추가,업데이트 진행중

--- finalMES/src/main/java/com/example/demo/produce/service/WorkService.java
+++ finalMES/src/main/java/com/example/demo/produce/service/WorkService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 
 import com.example.demo.produce.FacCateVO;
-
+import com.example.demo.produce.WorkMidRegistVO;
 
 public interface WorkService {
 	public List<FacCateVO> getFacSelect(String procCode);
+	public int insertWorkMidRegist(WorkMidRegistVO workMidRegistVO);
+	public List<WorkMidRegistVO> checkWorkMidRegist(String dinsCode, String procCode);
 }
